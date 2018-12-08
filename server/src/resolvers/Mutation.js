812551338508f9a6176dc414c09c5c94b0d23af0
Prateek Thapa <prateekthapa26@gmail.com>
2018-12-08 13:33:02 +0545

    changed prisma deploy hook

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,3 +1,16 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    console.log('inside the mutations => parent', parent);
+    console.log('inside the mutations => args', args);
+    console.log('inside the mutations => ctx', ctx);
+    console.log('inside the mutations => info', info);
+
+    const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
+
+    console.log(item);
+
+    return item;
+  },
+};
 
 module.exports = Mutations;
