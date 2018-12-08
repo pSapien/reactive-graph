d8d5a2b062b245fdb5086d90ba2f6a85fbc8c2de
Prateek Thapa <prateekthapa26@gmail.com>
2018-12-08 13:34:58 +0545

    removed console logs

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,14 +1,6 @@
 const Mutations = {
   async createItem(parent, args, ctx, info) {
-    console.log('inside the mutations => parent', parent);
-    console.log('inside the mutations => args', args);
-    console.log('inside the mutations => ctx', ctx);
-    console.log('inside the mutations => info', info);
-
     const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
-
-    console.log(item);
-
     return item;
   },
 };
