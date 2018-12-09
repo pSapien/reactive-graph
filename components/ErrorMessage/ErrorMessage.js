3acfc0798ef860e7be9e67e6ddc10adc0c894aba
Prateek Thapa <prateekthapa26@gmail.com>
2018-12-09 21:39:19 +0545

    changing the error message

--- components/ErrorMessage/ErrorMessage.js
+++ components/ErrorMessage/ErrorMessage.js
@@ -18,26 +18,14 @@ const ErrorStyles = styled.div`
 const DisplayError = ({ error = {} }) => {
   if (!error || !error.message) return null;
 
-  if (
-    error.networkError &&
-    error.networkError.result &&
-    error.networkError.result.errors.length
-  ) {
-    return error.networkError.result.errors.map((error, i) => (
-      <ErrorMessage message={error.message} key={i} />
-    ));
-  }
-
-  return <ErrorMessage error={error.message} />;
+  return (
+    <ErrorStyles>
+      <p data-test="graphql-error">
+        <strong>Shoot!</strong>
+        {error.message}
+      </p>
+    </ErrorStyles>
+  );
 };
-
-const ErrorMessage = ({ message }) => (
-  <ErrorStyles>
-    <p data-test="graphql-error">
-      <strong>Shoot!</strong>
-      {message.replace('GraphQL error: ', '')}
-    </p>
-  </ErrorStyles>
-);
 
 export default DisplayError;
