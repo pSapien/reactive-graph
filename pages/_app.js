04a80097e912112315777c35c801a1ea64129f64
Prateek Thapa <prateekthapa26@gmail.com>
2018-12-09 21:39:35 +0545

    passed page props

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,25 @@ import withData from '../lib/withData';
 import App, { Container } from 'next/app';
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
+
   render() {
-    const { Component, apollo } = this.props;
+    const { Component, apollo, pageProps } = this.props;
 
     return (
       <Container>
         <ApolloProvider client={apollo}>
           <Page>
-            <Component />
+            <Component {...pageProps} />
           </Page>
         </ApolloProvider>
       </Container>
