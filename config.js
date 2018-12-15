caa62f98edc2140fb678f8162e6ca9b757d2f349
Prateek Thapa <prateekthapa26@gmail.com>
2018-12-15 19:30:45 +0545

    using ip address instead of localhost in config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `http://localhost:4444`;
+export const endpoint = `http://192.168.100.15:4444`;
 export const perPage = 4;
 export const cloudinaryAPI =
   'https://api.cloudinary.com/v1_1/falcon-arrow/image/upload';
--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -3,12 +3,14 @@ const Mutations = {
     const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
     return item;
   },
+
   async updateItem(parent, args, ctx, info) {
     const updates = { ...args };
     const where = { id: args.id };
     delete updates.id;
     await ctx.db.mutation.updateItem({ data: updates, where }, info);
   },
+
   async deleteItem(parent, args, ctx, info) {
     const where = { id: args.id };
     return await ctx.db.mutation.deleteItem({ where });
